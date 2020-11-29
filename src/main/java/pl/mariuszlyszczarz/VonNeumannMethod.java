05be343b617432ab8c706bf972eb6797d6ef7b08
Mariusz94 <lyszczarz.mriusz@gmail.com>
2020-11-30 00:14:10 +0100

    Create class VonNeumannMethod.java

---
+++ src/main/java/pl/mariuszlyszczarz/VonNeumannMethod.java
@@ -0,0 +1,12 @@
+package pl.mariuszlyszczarz;
+
+import javafx.scene.control.CheckBox;
+
+import java.awt.image.BufferedImage;
+
+public class VonNeumannMethod extends GrainGrowthModel {
+    @Override
+    public BufferedImage implementationMethod(BufferedImage bufferedImage, CheckBox periodicCheckBox) {
+        return null;
+    }
+}
